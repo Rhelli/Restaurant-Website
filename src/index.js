9703af68fd850b4b9f4481c1e75cdcf746894efb
Rory Hellier <rory.hellier@googlemail.com>
2020-09-01 22:53:22 +0100

    Implemented working tab switch version. Now att empting to implement tab switch animations

--- src/index.js
+++ src/index.js
@@ -5,5 +5,38 @@ import '@fortawesome/fontawesome-free/js/brands';
 
 import './style.scss';
 import mainPageContainer from './loader';
+import generateNavbar from './nav';
+import generateHomepage from './home';
+import generateAbout from './about';
+import generateMenu from './menu';
 
 document.body.appendChild(mainPageContainer());
+const mainContainer = document.getElementById('content');
+mainContainer.appendChild(generateNavbar());
+mainContainer.appendChild(generateHomepage());
+
+
+  const navHomeButton = document.getElementById('navHomepageButton');
+  const navMenuButton = document.getElementById('navMenuButton');
+  const navAboutButton = document.getElementById('navAboutButton');
+
+  let navLinks = [navHomeButton, navMenuButton, navAboutButton];
+
+for (let i = 0; i < navLinks.length; i++) {
+  // eslint-disable-next-line no-loop-func
+  navLinks[i].addEventListener('click', (event) => {
+    const identifier = navLinks[i].id;
+    if (identifier === 'navHomepageButton') {
+      mainContainer.removeChild(mainContainer.lastChild);
+      mainContainer.appendChild(generateHomepage());
+    }
+    if (identifier === 'navMenuButton') {
+      mainContainer.removeChild(mainContainer.lastChild);
+      mainContainer.appendChild(generateMenu());
+    }
+    if (identifier === 'navAboutButton') {
+      mainContainer.removeChild(mainContainer.lastChild);
+      mainContainer.appendChild(generateAbout());
+    }
+  });
+}
--- src/loader.js
+++ src/loader.js
@@ -1,25 +1,7 @@
 import * as generator from './generator';
-import generateNavbar from './nav';
-import generateHomepage from './home';
-import generateAbout from './about';
-import generateMenu from './menu';
 
 export default function mainPageContainer() {
   // ASSEMBLE HOMEPAGE ELEMENTS
   const mainContainer = generator.htmlGenerator('div', 'main-container', 'content');
-  mainContainer.appendChild(generateNavbar());
-  mainContainer.appendChild(generateHomepage());
-
-  // BUILD DEFAULT HOMEPAGE / BUILD NEW PAGE ON CHANGE
-  const mainPageBuilder = (page) => {
-    if (page) {
-      const changePage = mainContainer;
-      changePage.removeChild(changePage.lastChild);
-      changePage.appendChild(page);
-      return changePage;
-    }
-    return mainContainer;
-  };
-
-  return mainPageBuilder(generateMenu());
+  return mainContainer;
 }
