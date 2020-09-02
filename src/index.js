340d13f72db8f8aacd7a9da51fec2ea00df1d703
Rory Hellier <rory.hellier@googlemail.com>
2020-09-02 19:01:38 +0100

    Refactored and Cleaned Up Index.js
     - Refactored the tab swapping behaviour and encapsulated in new functipn pageSwapHandle
     - Encapsulated the event listeners loop in an iffe
     - Converted the seperate initialize function and the initialize iffe call into one iffe

--- src/index.js
+++ src/index.js
@@ -10,65 +10,62 @@ import generateHomepage from './home';
 import generateAbout from './about';
 import generateMenu from './menu';
 
-document.body.appendChild(mainPageContainer());
+// INITIALIZE PAGE CONTAINER AND DEFAULT CONTENTS
+(function() {
+  document.body.appendChild(mainPageContainer());
+  const mainContainer = document.getElementById('content');
+  mainContainer.appendChild(generateNavbar());
+  mainContainer.appendChild(generateHomepage());
+}());
+
+// DEFINE NAV TABS & ARRAY
 const mainContainer = document.getElementById('content');
-mainContainer.appendChild(generateNavbar());
-mainContainer.appendChild(generateHomepage());
+const navHomeButton = document.getElementById('navHomepageButton');
+const navMenuButton = document.getElementById('navMenuButton');
+const navAboutButton = document.getElementById('navAboutButton');
+const navLinks = [navHomeButton, navMenuButton, navAboutButton];
 
-
-  const navHomeButton = document.getElementById('navHomepageButton');
-  const navMenuButton = document.getElementById('navMenuButton');
-  const navAboutButton = document.getElementById('navAboutButton');
-
-  let navLinks = [navHomeButton, navMenuButton, navAboutButton];
-for (let i = 0; i < navLinks.length; i++) {
-  // eslint-disable-next-line no-loop-func
-  navLinks[i].addEventListener('click', (event) => {
-    const identifier = navLinks[i].id;
-    if (identifier === 'navHomepageButton' && mainContainer.lastChild.id !== 'landingContainer') {
-      if (mainContainer.lastChild.id === 'menuContainer') {
-        let currentPage = mainContainer.lastChild;
-        currentPage.classList.removeChild('slide-in-right');
-        currentPage.classList.add('slide-out-right');
-      } else {
-        let currentPage = mainContainer.lastChild;
-        currentPage.classList.remove('slide-in-top');
-        currentPage.classList.add('slide-out-top');
-      }
+// DEFINE TAB SWAPPING & ANIMATION BEHAVIOUR
+const pageSwapHandle = (page) => {
+  const isolatedPageName = page.slice(3).split('').reverse().slice(6).reverse().join('').toLowerCase();
+  if (mainContainer.lastChild !== `${isolatedPageName}Container`) {
+    if (mainContainer.lastChild.id === 'menuContainer') {
+      const currentPage = mainContainer.lastChild;
+      currentPage.classList.remove('slide-in-right');
+      currentPage.classList.add('slide-out-right');
+    } else {
+      const currentPage = mainContainer.lastChild;
+      currentPage.classList.remove('slide-in-top');
+      currentPage.classList.add('slide-out-top');
+    }
+    if (isolatedPageName === 'homepage') {
       setTimeout(() => {
         mainContainer.removeChild(mainContainer.lastChild);
         mainContainer.appendChild(generateHomepage());
       }, 400);
     }
-    if (identifier === 'navMenuButton' && mainContainer.lastChild.id !== 'menuContainer') {
-      if (mainContainer.lastChild.id === 'menuContainer') {
-        let currentPage = mainContainer.lastChild;
-        currentPage.classList.removeChild('slide-in-right');
-        currentPage.classList.add('slide-out-right');
-      } else {
-        let currentPage = mainContainer.lastChild;
-        currentPage.classList.remove('slide-in-top');
-        currentPage.classList.add('slide-out-top');
-      }
+    if (isolatedPageName === 'menu') {
       setTimeout(() => {
         mainContainer.removeChild(mainContainer.lastChild);
         mainContainer.appendChild(generateMenu());
       }, 400);
     }
-    if (identifier === 'navAboutButton' && mainContainer.lastChild !== 'aboutContainer') {
-      if (mainContainer.lastChild.id === 'menuContainer') {
-        let currentPage = mainContainer.lastChild;
-        currentPage.classList.remove('slide-in-right');
-        currentPage.classList.add('slide-out-right');
-      } else {
-        let currentPage = mainContainer.lastChild;
-        currentPage.classList.remove('slide-in-top');
-        currentPage.classList.add('slide-out-top');
-      }
+    if (isolatedPageName === 'about') {
       setTimeout(() => {
         mainContainer.removeChild(mainContainer.lastChild);
         mainContainer.appendChild(generateAbout());
       }, 400);
     }
-  });
-}
+  }
+};
+
+// ADD TABS EVENT LISTENERS
+(function() {
+  for (let i = 0; i < navLinks.length; i++) {
+    // eslint-disable-next-line no-loop-func
+    navLinks[i].addEventListener('click', (event) => {
+      const identifier = navLinks[i].id;
+      pageSwapHandle(identifier);
+    });
+  }
+}());
